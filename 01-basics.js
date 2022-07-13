0488d6449cd404c2594472d304949f1647407ca7
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-07-13 16:27:39 +0300

    Автоперевірки + відео репети

---
+++ 01-basics.js
@@ -0,0 +1,25 @@
+/* Знакомство с масивами
+ */
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// console.table(friends);
+// узнать последний индекс - это всегда длина -1
+// const lastIndex = friends.length -1;
+// console.log(lastIndex);
+// когда вы хотите обратится к елементу всредине масива - вы ставите: 
+// console.log(friends[0]);
+// как что-то записать  в масив:
+// friends[1] = 'hfjhfjh'
+// friends[3] = 65465;
+// console.table(friends);
+
+
+
+/* Примитивные и сложные типы
+Передача по сылке и по значению
+Сылочное равенство
+ */
+let a = 10;
+let b = a;
+
+console.log(a);
+console.log(b);
