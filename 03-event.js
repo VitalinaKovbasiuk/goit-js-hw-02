52496ec081f18e3a378b9bb41f873b0558560ccd
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-07-13 21:00:34 +0300

    Розбір космпекту Репета ч4

--- 03-event.js
+++ 03-event.js
@@ -20,10 +20,23 @@
 // const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
 // let total = 0;
 // for (const number of numbers) {
-//     // console.log(number);
+    // console.log(number);
 //     if (number % 2 === 0) {
 //     console.log(`${number} - четное!!!`);
 //     total += number;
 //     }
+// }
+// console.log('Total:', total);
+
+// !!!от обатного - вместо этого можна сделать по другому - проверить все, если не подходит , то пропускаем
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
+// let total = 0;
+// for (const number of numbers) {
+//     if (number % 2 !== 0) {
+//     console.log('Этy итерацию нужно пропустить', number);
+//     continue;
+//     }
+//     console.log(`${number} - четное!!!`);
+//     total += number;
 // }
 // console.log('Total:', total);
--- 04-find-login.js
+++ 04-find-login.js
@@ -1,2 +1,39 @@
-/* 
+/* Напиши скрипт поиска логина
+Если логина нет - вывести сообщение 'Пользователь [логин] не найден.'
+Если логин есть - вывести сообщение 'Пользователь [логин] найден.'
  */
+
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'ajxth3m4n'];
+// const loginToFind = 'poly1scute';
+// let message = '';
+
+// for (let i = 0; i < logins.length; i += 1) {
+//     const login = logins[i];
+//     if (login === loginToFind) {
+//         message = `Пользователь ${loginToFind} найден.`;
+//         break;
+//     } 
+//     message = `Пользователь ${loginToFind}  не найден.`;
+// }
+// console.log(message);
+
+
+const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'ajxth3m4n'];
+const loginToFind = 'poly1scute';
+let message = '';
+
+for (let i = 0; i < logins.length; i += 1) {
+    const login = logins[i];
+
+    console.log('Login: ', login);
+
+    console.log(`${login} === ${loginToFind}: `, login === loginToFind);
+    if (login === loginToFind) {
+        console.log('Ура равны!!!!');
+        message = `Пользователь ${loginToFind} найден.`;
+        break;
+    } 
+
+    message = `Пользователь ${loginToFind}  не найден.`;
+}
+console.log(message);
