3f0c302056a8e0c0071f62756ba6016309a20c61
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-07-13 17:58:46 +0300

    Розбір космпекту Репета ч2

--- 01-basics.js
+++ 01-basics.js
@@ -18,8 +18,43 @@
 Передача по сылке и по значению
 Сылочное равенство
  */
-let a = 10;
-let b = a;
+// let a = 10;
+// let b = a;
+// console.log(a);
+// console.log(b);
+//  a = 20;
+// console.log(a);
+// console.log(b);
 
-console.log(a);
-console.log(b);
+// Передача по силці:
+// const a = [1, 2, 3];
+// const b = a;
+// console.log('a', a);
+// console.log('b', b);
+// a[0] = 500;
+// console.log('a', a);
+// console.log('b', b);
+// console.log(a === b);
+
+
+
+
+/* Перебор - интерация масива
+ */
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// console.table(friends);
+// const lastIndex = friends.length -1;
+
+// for(let i = 0; i <= lastIndex; i += 1) {
+//     console.log(friends[i]);
+// }
+// додати щось всередину масиву:
+// for(let i = 0; i <= lastIndex; i += 1) {
+//     friends[i] += '-1';
+// }
+// console.table(friends);
+ 
+// for.....of если вам не нужно изменять елемент масива или не нужен индекс
+// for (const friend of friends) {
+//     console.table(friend);
+// }
