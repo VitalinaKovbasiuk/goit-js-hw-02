67a617c65d8e300658d213366df74b2218f9ce62
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-07-15 12:29:06 +0300

    Розбір конспекта Репета ч5

--- 04-find-login.js
+++ 04-find-login.js
@@ -17,23 +17,49 @@
 // }
 // console.log(message);
 
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'ajxth3m4n'];
+// const loginToFind = 'poly1scute';
+// let message = '';
+// for (let i = 0; i < logins.length; i += 1) {
+//     const login = logins[i];
+//     console.log('Login: ', login);
+//     console.log(`${login} === ${loginToFind}: `, login === loginToFind);
+//     if (login === loginToFind) {
+//         console.log('Ура равны!!!!');
+//         message = `Пользователь ${loginToFind} найден.`;
+//         break;
+//     } 
+//     message = `Пользователь ${loginToFind}  не найден.`;
+// }
+// console.log(message);
 
-const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'ajxth3m4n'];
-const loginToFind = 'poly1scute';
-let message = '';
+// !або це можна запісаті ось так:
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'ajxth3m4n'];
+// const loginToFind = 'poly1scute';
+// let message = `Пользователь ${loginToFind}  не найден.`;
 
-for (let i = 0; i < logins.length; i += 1) {
-    const login = logins[i];
+// for (const login of logins) {
+//     console.log('Login: ', login);
+//     console.log(`${login} === ${loginToFind}: `, login === loginToFind);
 
-    console.log('Login: ', login);
+//     if (login === loginToFind) {
+//         console.log('Ура равны!!!!');
+//         message = `Пользователь ${loginToFind} найден.`;
+//         break;
+//     } 
+// }
+// console.log(message);
 
-    console.log(`${login} === ${loginToFind}: `, login === loginToFind);
-    if (login === loginToFind) {
-        console.log('Ура равны!!!!');
-        message = `Пользователь ${loginToFind} найден.`;
-        break;
-    } 
+// !!!!!!!!є ще самий простий варіант з include :
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'ajxth3m4n'];
+// const loginToFind = 'poly1scute';
+// let message = `Пользователь ${loginToFind}  не найден.`;
+// console.log(logins.includes(loginToFind));
 
-    message = `Пользователь ${loginToFind}  не найден.`;
-}
-console.log(message);
+
+// на пальцях пояснюю деклоративный код - абстракция (метод масива, 
+// который под капотом скрывает детали реализации): 
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'ajxth3m4n'];
+// const loginToFind = 'poly1scute';
+// const message = logins.includes(loginToFind) ? `Пользователь ${loginToFind} найден.` : `Пользователь ${loginToFind}  не найден.`;
+// console.log(message);
